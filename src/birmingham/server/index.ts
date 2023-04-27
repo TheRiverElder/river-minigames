248f064002e5e8c89219a5421ab9168c128c2233
TheRiverElder <765375399@qq.com>
2023-04-27 19:39:32 +0800

    塞服务器

--- src/birmingham/server/index.ts
+++ src/birmingham/server/index.ts
@@ -5,10 +5,16 @@ import { Nullable } from "../../libs/lang/Optional";
 import Game from "../Game";
 import Player from "../Player";
 import { initializeBirmingham } from "../test";
+import { WebSocket } from "ws";
 
 let game: Nullable<Game> = null;
 
-const playerIdToInstanceMap: Map<string, Player> = new Map();
+const playerIdToInstanceMap: Map<int, Player> = new Map();
+const playerIdToClientMap: Map<int, WebSocket> = new Map();
+
+function broadcast(message: string) {
+    Array.from(playerIdToClientMap.values()).forEach(client => client.send(message));
+}
 
 const rawApp = express();
 expressWS(rawApp);
@@ -23,16 +29,38 @@ app.post("/birmingham/initialize", (request, response) => {
     }
     const playerAmount: int = parseInt(playerAmountStr);
     game = initializeBirmingham(playerAmount);
+    playerIdToInstanceMap.clear();
+    game.players.forEach((player, i) => playerIdToInstanceMap.set(i, player));
     response.send("Done!");
 });
 
 app.ws("/birmingham/run", (ws, request) => {
-    const playerId = request.params["playerId"];
+    const playerId = parseInt(request.params["playerId"]);
     if (!playerId) ws.close(400, "No playerId!");
+
+    const player = playerIdToInstanceMap.get(playerId);
+    if (!player) {
+        ws.close(400, `No player with playerId=${playerId}!`);
+        return;
+    }
+
+    ws.onopen = () => {
+        playerIdToClientMap.set(playerId, ws);
+        broadcast(`${player.name}(#${playerId})加入了游戏`)
+    };
 
     ws.onmessage = (event) => {
         const data: string = event.data.toString();
         console.log("receive: ", data);
+    };
+
+    ws.onclose = () => {
+        playerIdToClientMap.delete(playerId);
+        broadcast(`${player.name}(#${playerId})离开了游戏`)
+    };
+
+    ws.onerror = () => {
+        
     };
 });
 
