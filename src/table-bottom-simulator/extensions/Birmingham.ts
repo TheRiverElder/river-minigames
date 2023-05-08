1d9cd46b118cbc6f2c0a07531efc08ef4c2edb82
TheRiverElder <765375399@qq.com>
2023-05-08 17:22:50 +0800

    试图加入伯明翰拓展，但是似乎先要完成堆叠相关的内容

--- src/table-bottom-simulator/extensions/Birmingham.ts
+++ src/table-bottom-simulator/extensions/Birmingham.ts
@@ -1,5 +1,11 @@
+import { int } from "../../libs/CommonTypes";
+import Registry from "../../libs/management/Registry";
 import TableBottomSimulatorClient from "../TableBottomSimulatorClient";
+import BirminghamPlayer from "./BirminghamPlayer";
+
+export const REGISTRY_PLAYER = new Registry<int, BirminghamPlayer>(player => player.user.uid);
 
 // 只是注册一些主要的BehaviorType
 export default function initializeBirminghamBasic(simulator: TableBottomSimulatorClient) {
+    
 }
