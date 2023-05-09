a1b30c22a7333e925b844bd2297f6a9ca587f4c6
TheRiverElder <765375399@qq.com>
2023-05-09 23:49:17 +0800

    改进NumberInput

--- src/libs/ui/NumberInput.tsx
+++ src/libs/ui/NumberInput.tsx
@@ -8,7 +8,7 @@ export function NumberInput(props: CommonInputProps<number>) {
             className={props.className}
             style={props.style}
             value={props.value}
-            onChange={e => props.onChange(parseFloat(e.target.value))}
+            onChange={e => props.onChange(parseFloat(e.target.value) || 0)}
         />
     )
 }
