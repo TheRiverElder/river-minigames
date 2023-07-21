f7b9ab50a5edfeb6bcfa62fce8bd19731015bf93
TheRiverElder <765375399@qq.com>
2023-07-21 22:19:18 +0800

    OG：新建

---
+++ src/osmos/ui/OsmosView.tsx
@@ -0,0 +1,12 @@
+import { Component, ReactNode } from "react";
+import "./OsmosView.scss";
+
+export default class OsmosView extends Component {
+    render(): ReactNode {
+        return (
+            <div className="OsmosView">
+
+            </div>
+        );
+    }
+}
